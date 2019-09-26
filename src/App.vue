c7e205cbe026509caa1cc110ed4740162b45e41e
mlevi <mlevi@mdinonline.ru>
2019-09-27 00:00:23 +0300

    Application example

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vue-starfall height="100%" width="100%" user-feedback>
+      <img alt="Vue logo" src="./assets/logo.png" class="logo">
+    </vue-starfall>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VueStarfall from './components/VueStarfall.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    VueStarfall
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html, body, #app {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+  .logo {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
 </style>
